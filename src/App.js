3d06bbc870b99a8f0ad1bfb97041cfdd355ca63d
Paul Vidler <45616638+PaulVidler@users.noreply.github.com>
2020-06-27 15:26:23 +1000

    1st-commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import './App.css';
+import Home from './Components';
+
+function App() {
+  return (
+    <>
+    <CssBaseline />
+    <Home />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {makeStyles} from "@material-ui/core/styles"
+import {Typography, Avatar, Grid, Box} from '@material-ui/core'
+import avatar from "../Images/avatar.png"
+import Typed from "react-typed"
+
+
+// css styles
+const useStyles = makeStyles(
+    theme=>({
+        avatar: {
+            width: theme.spacing(15),
+            height: theme.spacing(15),
+            margin: theme.spacing(1),
+        },
+        title: {
+            color: "tomato"
+        },
+        subtitle: {
+            color: "tan",
+            marginBottom: "3rem"
+        },
+        typedContainer: {
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: "100vw",
+            textAlign: "center",
+            zIndex: 1
+        }
+    })
+);
+
+
+const Header = () => {
+    const classes = useStyles();
+    return (
+        <Box className={classes.typedContainer}>
+            <Grid container justify="center">
+                <Avatar className={classes.avatar} src={avatar} alt="Paul" />
+            </Grid>
+            <Typography className={classes.title}variant="h4">
+                <Typed strings={["Paul Vidler"]}
+                typeSpeed={40} />
+            </Typography>
+            <br />
+            <Typography className={classes.subtitle} variant="h5">
+                <Typed strings={["Web Design", "Web development", ".NET Framework"]}
+                typeSpeed={40}
+                backSpeed={60} loop />
+            </Typography>
+        </Box>
+    )
+}
+
+export default Header
